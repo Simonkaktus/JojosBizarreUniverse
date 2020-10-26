df7a4f39674b8122058b9650781798e31a999352
Simonkaktus <73407108+Simonkaktus@users.noreply.github.com>
2020-10-26 14:35:22 +0100

    NEfgeyGH

---
+++ Jojo's bizarre dimensions/Assets/Scripts/NetworkEvent.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Bolt;
+
+public class NetworkEvent : MonoBehaviour
+{
+       public void  OnConnectedToMaster()
+        {
+            CustomEvent.Trigger(this.gameObject, "OnConnectedToMaster");
+        }
+    public void OnJoinedLobby()
+    {
+        CustomEvent.Trigger(this.gameObject, "OnJoinedLobby");
+    }
+}
